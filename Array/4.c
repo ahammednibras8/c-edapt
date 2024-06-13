8fabfe9a151d6bc053c53347660c5d05ade3e782
ahammed-nibras <ahammednibras@proton.me>
2024-06-13 10:23:17 +0530

    Merge two arrays and print the merged one.

--- Array/4.c
+++ Array/4.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 
 int main(void) {
-    int array1[3] = {1, 2, 3};
-    int array2[3] = {10, 20, 30};
-    int merged_array[6];
+    int array1[] = {1, 2, 3};
+    int array2[] = {10, 20, 30};
 
-    for (int i = 0; i < 6; i++)
+    int size1 = 3;
+    int size2 = 3;
+    int merged_size = size1 + size2;
+
+    int merged_array[merged_size];
+
+    for (int i = 0; i < size1; i++)
     {
-        if(i < 3) {
-            merged_array[6] = array1[i];
-        }
-        if(i > 3) {
-            merged_array[6] = array2[i]
-        }
+        merged_array[i] = array1[i];
     }
     
+    for (int i = 0; i < size2; i++)
+    {
+        merged_array[size1 + i] = array2[i];
+    }
+
+    printf("Merged Array: ");
+    for(int i = 0; i < merged_size; i++) {
+        printf("%i ", merged_array[i]);
+    }
+    printf("\n");
 }
